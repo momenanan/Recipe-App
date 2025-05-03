8000a504654c0f342c2b6a905436108bb61cf491
OsamaDW <s12028609@stu.najah.edu>
2025-05-04 01:01:32 +0300

    Sign up page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,12 @@
 import { Button, Typography, Divider } from "@mui/material";
-import {useRef} from "react";
+import {useRef, useState} from "react";
 import { FaFacebook, FaGoogle } from "react-icons/fa";
 import {CustomButton} from "../CustomButton.jsx";
 import {FormInput} from "../FormInput.jsx";
 import {CheckBox} from "../CheckBox.jsx";
 import {useNavigate} from "react-router-dom";
 
-export default function SignUpPage() {
+export default function LoginPage() {
     const navigate = useNavigate();
     const submitRef = useRef(null);
     const passwordRef = useRef(null);
@@ -16,7 +16,7 @@ export default function SignUpPage() {
             <div className="max-w-sm w-full bg-white rounded-xl shadow-md p-6 text-center space-y-4">
                 <img src="./logo.svg" alt="Cook Eat It" className="mx-auto w-24" />
 
-                <Typography variant="h6" className="text-gray-700">Continue with</Typography>
+                <Typography variant="h6" className="text-gray-700">Sign In with</Typography>
 
                 <div className="flex gap-3 justify-center">
                     <Button variant="outlined" startIcon={<FaFacebook />} className="capitalize w-1/2">
@@ -30,7 +30,7 @@ export default function SignUpPage() {
                 <Divider>or</Divider>
 
                 <Typography variant="subtitle1" className="font-semibold text-gray-700">
-                    Create Your Account
+                    Login to your Account
                 </Typography>
 
                 <form className="space-y-4">
@@ -57,16 +57,16 @@ export default function SignUpPage() {
                         <CheckBox label="remember me" id="remember" size="small" />
                     </div>
 
-                <CustomButton ref={submitRef} type="submit" color="primary" className="w-full" >
-                    Sign up
-                </CustomButton>
+                    <CustomButton ref={submitRef} type="submit" color="primary" className="w-full" >
+                        Login
+                    </CustomButton>
                 </form>
 
                 <Typography variant="body2" className="text-gray-500">
-                    Already have an account? <a href="#" onClick={(e)=>{
-                    e.preventDefault();
-                    navigate('/');
-                }} className="text-[var(--primary)] underline">Login</a>
+                    Don't have an account? <a href="#" onClick={(e)=>{
+                        e.preventDefault();
+                        navigate('/signup');
+                }} className="text-[var(--primary)] underline">Sign UP</a>
                 </Typography>
             </div>
         </div>
